808ef74596da0b1a35dcc2b248ce2209e471c2f2
Stephen Sarquah <stephen.sarquah@gmail.com>
2019-09-19 13:10:15 +0200

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { RouterModule } from '@angular/router';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 @NgModule({
-  declarations: [AppComponent, ProjectsComponent],
+  declarations: [AppComponent, ProjectsComponent, ProjectDetailsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([{ path: '', component: ProjectsComponent }])
+    RouterModule.forRoot([
+      { path: '', component: ProjectsComponent },
+      { path: 'projects/:id', component: ProjectDetailsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
