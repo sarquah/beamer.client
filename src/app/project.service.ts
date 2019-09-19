8a5eb204b80204863e33c8798d3b56abe8f1f577
Stephen Sarquah <stephen.sarquah@gmail.com>
2019-09-19 17:38:52 +0200

    Added connection to backend

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { IProject } from './iproject';
-import { projects } from './projects';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
-  projects = projects;
+  projects = [];
+  baseUrl = 'http://localhost:65100/api/v1/project';
+
+  constructor(private httpClient: HttpClient) {}
 
   addProject(project: IProject) {
     this.projects.push(project);
   }
 
   getProjects() {
-    return this.projects;
+    return this.httpClient.get(`${this.baseUrl}/projects`);
+  }
+
+  getProject(id: number) {
+    return this.httpClient.get(`${this.baseUrl}/${id}`);
   }
   // Todo: Add methods for updating and deleting projects
 }
