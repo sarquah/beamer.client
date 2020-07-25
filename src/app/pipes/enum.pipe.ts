52dd95160db69fafc7fd0f52f89b5c809e86cb30
Stephen Sarquah <stephen.sarquah@gmail.com>
2020-07-25 10:38:53 +0200

    Added enum pipe (#16)

---
+++ src/app/pipes/enum.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'enum',
+})
+export class EnumPipe implements PipeTransform {
+  transform(value: string): string {
+    if (value) {
+      const resultArray = value.match(/[A-Z][a-z]+/g);
+      return resultArray.reduce((total, tmpString) => {
+        return total
+          ? `${total} ${tmpString.toLocaleLowerCase()}`
+          : `${tmpString}`;
+      });
+    }
+  }
+}
