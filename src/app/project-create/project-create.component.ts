eef3261ed7392ad4ea45c6609524ff85ef7fbd88
Stephen Sarquah <stephen.sarquah@gmail.com>
2021-01-07 14:47:07 +0100

    Added list of users when creating project (#20)
    Co-authored-by: Stephen Kow Sarquah <SCJ@kmd.dk>

--- src/app/project-create/project-create.component.ts
+++ src/app/project-create/project-create.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { IProject } from '../models/interfaces/IProject';
 import { Router } from '@angular/router';
 import { ProjectService } from '../services/project.service';
 import { EStatus } from '../models/enums/EStatus';
+import { UserService } from '../services/user.service';
+import { IUser } from '../models/interfaces/IUser';
 
 @Component({
   selector: 'app-project-create',
@@ -14,11 +16,13 @@ import { EStatus } from '../models/enums/EStatus';
 export class ProjectCreateComponent implements OnInit, OnDestroy {
   public form: FormGroup;
   public EStatus = EStatus;
+  public user$: Observable<IUser[]>;
   private subscriptions: Subscription[];
 
   constructor(
     private router: Router,
-    private projectService: ProjectService
+    private projectService: ProjectService,
+    private userService: UserService
   ) {}
 
   public ngOnDestroy() {
@@ -30,8 +34,12 @@ export class ProjectCreateComponent implements OnInit, OnDestroy {
   }
 
   public ngOnInit() {
+    this.user$ = this.userService.getUsers();
     this.form = this.projectService.createForm();
     this.subscriptions = [];
+    this.subscriptions.push(
+      this.user$.subscribe()
+    );
   }
 
   public createProject() {
