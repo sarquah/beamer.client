7c18a5e7d589ff6f5e536e474886b2db4a8c9ecb
Stephen Sarquah <stephen.sarquah@gmail.com>
2021-01-23 16:46:00 +0100

    Added authentication with Azure AD

--- src/core/models/auth-state-model.ts
+++
@@ -1,8 +0,0 @@
-import { IAuthTokenModel } from './auth-token-model';
-import { ProfileModel } from './profile-model';
-
-export interface AuthStateModel {
-  token?: IAuthTokenModel;
-  profile?: ProfileModel;
-  authReady?: boolean;
-}
--- src/core/models/register-model.ts
+++
@@ -1,5 +0,0 @@
-﻿export interface IRegisterModel {
-    username: string;
-    password: string;
-    confirmPassword: string;
-}
