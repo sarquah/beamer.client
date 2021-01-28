e937ddb2926dd5a74be324b2525c3cba8eac30ab
Stephen Sarquah <stephen.sarquah@gmail.com>
2021-01-28 22:49:54 +0100

    Feature/test adminservice (#33)
    * Updated karma
    
    * Changed esnext. Removed spec files. Added test
    
    * Changed coverage and added tests to adminservice

--- karma.conf.ts
+++ karma.conf.ts
@@ -1,32 +1,45 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = (config) => {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-coverage'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/beamer.client'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'lcov',
+      dir : 'coverage/'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'coverage'],
+    thresholds: {
+      statements: 80,
+      lines: 80,
+      branches: 80,
+      functions: 80
+    },
+    fixWebpackSourcePaths: true,
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'src/**/*.ts': ['coverage']
+    },
   });
 };
--- src/app/services/admin.service.spec.ts
+++ src/app/services/admin.service.spec.ts
@@ -1,16 +1,45 @@
 import { TestBed } from '@angular/core/testing';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AdminService } from './admin.service';
 
 describe('AdminService', () => {
-  let service: AdminService;
+  let sut: AdminService;
+  let formBuilder: FormBuilder = new FormBuilder();
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AdminService);
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        AdminService,
+        FormBuilder
+      ]
+    });
+    sut = TestBed.inject(AdminService);
+    formBuilder = TestBed.inject(FormBuilder);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(sut).toBeTruthy();
+  });
+
+  it('#getGroups should return a value', () => {
+    sut.getGroups().subscribe(data => expect(data).toEqual('Mock return value'));
+  });
+
+  it('#getGroupMembers should return a value', () => {
+    sut.getGroupMembers('testId').subscribe(data => expect(data).toEqual('Mock return value'));
+  });
+
+  it('#createForm should return a form', () => {
+    const formGroup = new FormGroup({
+      userGroupId: new FormControl('', Validators.required),
+      adminGroupId: new FormControl('', Validators.required)
+    });
+    expect(sut.createForm().value).toEqual(formGroup.value);
   });
 });
