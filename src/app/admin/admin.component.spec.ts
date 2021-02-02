9f44ebd22a5dfe0d5566a0e648cab21a248cfa4d
Stephen Sarquah <stephen.sarquah@gmail.com>
2021-02-02 16:40:21 +0100

    Fixed test

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -1,23 +1,13 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { fakeAsync, flush, TestBed, tick, waitForAsync } from '@angular/core/testing';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
+import { TestBed } from '@angular/core/testing';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MsalService } from '@azure/msal-angular';
 import { Observable } from 'rxjs';
-import { MSALInstanceFactory } from '../app.module';
 import { IUser } from '../models/interfaces/IUser';
 import { AdminService } from '../services/admin.service';
 import { MockAuthService } from '../services/mockauth.service.spec';
 import { UserService } from '../services/user.service';
 import { AdminComponent } from './admin.component';
-
-const users: IUser[] = [{
-    id: 0,
-    name: 'name',
-    department: 'department',
-    role: 'rolee',
-    tenantId: 'tenantId',
-    email: 'email'
-}];
 
 describe('AdminComponent', () => {
     let sut: AdminComponent;
@@ -28,6 +18,56 @@ describe('AdminComponent', () => {
         'createForm'
     ]);
     let userServiceMock = jasmine.createSpyObj<UserService>('UserService', ['createUsers']);
+    const users: IUser[] = [{
+        id: 0,
+        name: 'name',
+        department: 'department',
+        role: 'rolee',
+        tenantId: 'tenantId',
+        email: 'email'
+    }];
+
+    const createAdminServiceMock = (adminService: jasmine.SpyObj<AdminService>, formBuilder: FormBuilder, usersMock: IUser[]):
+        jasmine.SpyObj<AdminService> => {
+        const groupMembers = {
+            value: usersMock
+        };
+
+        const getGroupMembersReturn: Observable<any> = new Observable((observer) => {
+            observer.next(groupMembers);
+            observer.complete();
+        });
+        adminService.getGroupMembers.and.returnValue(getGroupMembersReturn);
+
+        const getGroupsReturn: Observable<any> = new Observable((observer) => {
+            observer.next('getGroups');
+            observer.complete();
+        });
+        adminService.getGroups.and.returnValue(getGroupsReturn);
+
+        const createFormReturn = formBuilder.group({
+            userGroupId: new FormControl('', Validators.required),
+            adminGroupId: new FormControl('', Validators.required)
+        });
+        adminService.createForm.and.returnValue(createFormReturn);
+        return adminService;
+    }
+
+    const createUserServiceThrowErrorMock = (userService: jasmine.SpyObj<UserService>):
+        jasmine.SpyObj<UserService> => {
+            userService.createUsers.and.throwError('Introduced error in unit test');
+        return userService
+    }
+
+    const createUserServiceMock = (userService: jasmine.SpyObj<UserService>, usersMock: IUser[]):
+        jasmine.SpyObj<UserService> => {
+        const users$: Observable<IUser[]> = new Observable((observer) => {
+            observer.next(usersMock);
+            observer.complete();
+        });
+        userService.createUsers.and.returnValue(users$);
+        return userService
+    }
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -54,7 +94,11 @@ describe('AdminComponent', () => {
         });
         sut = TestBed.inject(AdminComponent);
         formBuilderMock = TestBed.inject(FormBuilder);
-        adminServiceMock = createAdminServiceMock(adminServiceMock, formBuilderMock);
+        adminServiceMock = createAdminServiceMock(adminServiceMock, formBuilderMock, users);
+    });
+
+    afterAll(() => {
+        TestBed.resetTestingModule();
     });
 
     it('should be created', () => {
@@ -105,7 +149,7 @@ describe('AdminComponent', () => {
 
     describe('#sync side effects', () => {
         it('should be called', () => {
-            userServiceMock = createUserServiceMock(userServiceMock);
+            userServiceMock = createUserServiceMock(userServiceMock, users);
             const formGroup = formBuilderMock.group({
                 userGroupId: new FormControl('userGroupId', Validators.required),
                 adminGroupId: new FormControl('adminGroupId', Validators.required)
@@ -134,7 +178,7 @@ describe('AdminComponent', () => {
         });
 
         it('adminService should return data', () => {
-            userServiceMock = createUserServiceMock(userServiceMock);
+            userServiceMock = createUserServiceMock(userServiceMock, users);
             const formGroup = formBuilderMock.group({
                 userGroupId: new FormControl('userGroupId', Validators.required),
                 adminGroupId: new FormControl('adminGroupId', Validators.required)
@@ -155,45 +199,3 @@ describe('AdminComponent', () => {
         });
     });
 });
-
-function createAdminServiceMock(adminServiceMock: jasmine.SpyObj<AdminService>, formBuilderMock: FormBuilder):
-    jasmine.SpyObj<AdminService> {
-    const groupMembers = {
-        value: users
-    };
-
-    const getGroupMembersReturn: Observable<any> = new Observable((observer) => {
-        observer.next(groupMembers);
-        observer.complete();
-    });
-    adminServiceMock.getGroupMembers.and.returnValue(getGroupMembersReturn);
-
-    const getGroupsReturn: Observable<any> = new Observable((observer) => {
-        observer.next('getGroups');
-        observer.complete();
-    });
-    adminServiceMock.getGroups.and.returnValue(getGroupsReturn);
-
-    const createFormReturn = formBuilderMock.group({
-        userGroupId: new FormControl('', Validators.required),
-        adminGroupId: new FormControl('', Validators.required)
-    });
-    adminServiceMock.createForm.and.returnValue(createFormReturn);
-    return adminServiceMock;
-}
-
-function createUserServiceThrowErrorMock(userServiceMock: jasmine.SpyObj<UserService>):
-    jasmine.SpyObj<UserService> {
-    userServiceMock.createUsers.and.throwError('Introduced error in unit test');
-    return userServiceMock
-}
-
-function createUserServiceMock(userServiceMock: jasmine.SpyObj<UserService>):
-    jasmine.SpyObj<UserService> {
-    const users$: Observable<IUser[]> = new Observable((observer) => {
-        observer.next(users);
-        observer.complete();
-    });
-    userServiceMock.createUsers.and.returnValue(users$);
-    return userServiceMock
-}
