354838c9d456a8a0eeaa8ed93e7b3d6af764d712
Stephen Sarquah <stephen.sarquah@gmail.com>
2021-02-02 17:33:28 +0100

    Moved syncGroup to adminService

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { switchMap, catchError } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+import { IUser } from '../models/interfaces/IUser';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +14,8 @@ export class AdminService {
 
   constructor(
     private formBuilder: FormBuilder,
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private userService: UserService
   ) { }
 
   public getGroups(): Observable<any> {
@@ -28,5 +32,26 @@ export class AdminService {
       adminGroupId: new FormControl('', Validators.required)
     };
     return this.formBuilder.group(formControls);
+  }
+
+  public syncGroup(userGroupId: string, tenantId: string): Observable<IUser[]> {
+    return this.getGroupMembers(userGroupId).pipe(
+      switchMap(groupMembers => {
+        const users: IUser[] = groupMembers.value.map(member => {
+          return {
+            name: member.displayName,
+            department: '',
+            role: member.jobTitle,
+            tenantId,
+            email: member.mail
+          }
+        });
+        return this.userService.createUsers(users)
+      }),
+      catchError(error => {
+        console.error(error);
+        return throwError(error);
+      })
+    )
   }
 }
