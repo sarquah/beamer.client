287faa846ab8b48862c17a3fdfaea8e3fa65d0ab
Stephen Sarquah <stephen.sarquah@gmail.com>
2021-02-03 16:33:42 +0100

    Fixed test for project create component

--- src/app/project-create/project-create.component.spec.ts
+++ src/app/project-create/project-create.component.spec.ts
@@ -3,6 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MsalService } from '@azure/msal-angular';
+import { Observable } from 'rxjs';
+import { EStatus } from '../models/enums/EStatus';
+import { IProject } from '../models/interfaces/IProject';
 import { MockAuthService } from '../services/mockauth.service.spec';
 import { ProjectService } from '../services/project.service';
 import { UserService } from '../services/user.service';
@@ -11,9 +14,15 @@ import { ProjectCreateComponent } from './project-create.component';
 describe('ProjectCreateComponent', () => {
     let sut: ProjectCreateComponent;
     let formBuilderMock: FormBuilder;
+    let projectServiceMock: jasmine.SpyObj<ProjectService>;
 
     beforeEach(() => {
         const routerMock = jasmine.createSpyObj<Router>('Router', ['navigate']);
+        projectServiceMock = jasmine.createSpyObj<ProjectService>('ProjectService', [
+            'createProject',
+            'createForm'
+        ]);
+
         TestBed.configureTestingModule({
             imports: [
                 HttpClientTestingModule,
@@ -21,7 +30,6 @@ describe('ProjectCreateComponent', () => {
             ],
             providers: [
                 ProjectCreateComponent,
-                ProjectService,
                 UserService,
                 FormBuilder,
                 {
@@ -31,11 +39,55 @@ describe('ProjectCreateComponent', () => {
                 {
                     provide: MsalService,
                     useClass: MockAuthService
+                },
+                {
+                    provide: ProjectService,
+                    useValue: projectServiceMock
                 }
             ]
         });
         sut = TestBed.inject(ProjectCreateComponent);
         formBuilderMock = TestBed.inject(FormBuilder);
+
+        const mockProject: IProject = {
+            id: 0,
+            tenantId: 'tenantId',
+            status: EStatus.NotStarted,
+            description: 'Project created for unit test',
+            ownerId: 0,
+            name: 'Unit test',
+            startDate: new Date(),
+            endDate: new Date(),
+            tasks: [
+                {
+                    id: 0,
+                    tenantId: 'tenantId',
+                    status: EStatus.NotStarted,
+                    description: 'Task created for unit test',
+                    ownerId: 0,
+                    name: 'Create unit test',
+                    startDate: new Date(),
+                    endDate: new Date(),
+                }
+            ]
+        };
+
+        const createProject$ = new Observable<IProject>((observer) => {
+            observer.next(mockProject);
+            observer.complete();
+        });
+        projectServiceMock.createProject.and.returnValue(createProject$);
+
+        const form = formBuilderMock.group({
+            name: new FormControl(null),
+            description: new FormControl(null),
+            ownerId: new FormControl(null),
+            startDate: new FormControl(null),
+            endDate: new FormControl(null),
+            status: new FormControl(null)
+        });
+        projectServiceMock.createForm.and.returnValue(form);
+
         sut.ngOnInit();
     });
 
@@ -48,15 +100,21 @@ describe('ProjectCreateComponent', () => {
     });
 
     describe('#createProject', () => {
-        it('Call with valid form', () => {
+        it('call with valid form', () => {
             sut.createProject();
             expect(sut.form.valid).toBeTrue();
+            expect(projectServiceMock.createProject.calls.count()).toEqual(1);
         });
 
-        it('Call with invalid form', () => {
+        it('call with invalid form', () => {
             sut.form.setErrors({ error: true })
             sut.createProject();
             expect(sut.form.invalid).toBeTrue();
+        });
+
+        it('called once', () => {
+            sut.createProject();
+            expect(projectServiceMock.createProject.calls.count()).toEqual(1);
         });
     });
 });
