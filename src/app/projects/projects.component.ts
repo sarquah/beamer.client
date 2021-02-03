af1ca761acbe0ee2e96444d6df0433b2b51ceac0
Stephen Sarquah <stephen.sarquah@gmail.com>
2021-02-03 17:39:53 +0100

    Added test for projects component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IProject } from '../models/interfaces/IProject';
-import { EStatus } from '../models/enums/EStatus';
 import { ProjectService } from '../services/project.service';
 
 @Component({
@@ -11,7 +10,6 @@ import { ProjectService } from '../services/project.service';
 })
 export class ProjectsComponent implements OnInit {
   public projects$: Observable<IProject[]>;
-  public EStatus = EStatus;
 
   constructor(
     private projectService: ProjectService
